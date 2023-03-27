b59ea9ab027eca85685f3afe270f5e51757e397a
Kim Yongjun <119988634+yjk490@users.noreply.github.com>
2023-03-27 23:18:52 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.08 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(int[] numbers) {
+        int[] answer = new int[numbers.length];
+        
+        if (numbers.length > 1000) {
+            return answer;
+        }
+        
+	    for (int i=0 ; i < numbers.length ; i++) {
+	    	if (Math.abs(numbers[i]) > 10000) {
+	        		return answer;
+	        	}
+	        	
+	    	answer[i] = numbers[i]*2;
+	    }
+        
+        return answer;   
+    }
+}
